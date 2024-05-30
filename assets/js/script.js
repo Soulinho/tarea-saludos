d57ff017a3e25739805c6179e36a4bdccb374b43
Soulinho <150733114+Soulinho@users.noreply.github.com>
2024-05-30 17:45:17 -0400

    subida

---
+++ assets/js/script.js
@@ -0,0 +1,59 @@
+// Carrusel modificado
+
+const miCarrusel = document.querySelector('#carouselExample')
+
+const carousel = new bootstrap.Carousel(miCarrusel, {
+    ride: true,
+    interval : 3000,
+    pause: false
+})
+
+miCarrusel.addEventListener('slide.bs.carousel', function(){
+    console.log('Se muestra un slide');
+    //document.getElementById('veces').innerHTML = (event.relatedTarget).length
+    //document.getElementById('ingresarAlgo').innerHTML += '<p>Ha pasado el slide.</p>'
+});
+
+//funcion modal para manipular los eventos 
+
+const miVentanaEmergente = document.getElementById('ventanaEmergente')
+
+// agregar un observador sobre el modal 
+
+
+miVentanaEmergente.addEventListener('show.bs.modal', function(event) {
+    console.log('se esta abriendo el modal')
+
+    // reconocer el elemento que habilita el evento modal
+    let boton = event.relatedTarget  // etiquetado relacionado 
+
+    let mensaje = boton.getAttribute('data-titulo')
+
+    let contenido = boton.getAttribute('data-contenido')
+
+
+
+    document.getElementById('tituloModal').innerText = mensaje
+    document.querySelector('.modal-body').innerHTML= contenido
+    
+})
+
+miVentanaEmergente.addEventListener('shown.bs.modal', function() {
+    console.log('esta abierto el modal')
+})
+
+
+miVentanaEmergente.addEventListener('hide.bs.modal', function() {
+    console.log('se esta cerrando el modal')
+})
+
+miVentanaEmergente.addEventListener('hidden.bs.modal', function() {
+    console.log('esta cerrado el modal')
+})
+
+
+
+
+
+
+
